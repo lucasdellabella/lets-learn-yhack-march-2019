af9fd66f754422f45d7d8dae7e1d09575ddd7a11
Lucas Della Bella <dellabella.lucas@gmail.com>
2019-03-28 14:30:10 -0700

    Add dummy propose an event button+form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import posts from "./mocks";
+import { Formik } from "formik";
 
 import "./styles.css";
 
@@ -8,10 +9,41 @@ function App() {
   return (
     <div className="App">
       <h1>Let's Learn</h1>
+      <ProposeMeetup className="proposal" />
       <Posts posts={posts} />
     </div>
   );
 }
+
+const ProposeMeetup = ({ className }) => {
+  const [opened, setOpened] = useState(false);
+  return !opened ? (
+    <button className={className} onClick={() => setOpened(true)}>
+      Propose an event!
+    </button>
+  ) : (
+    <Formik
+      initialValues={{ title: "my title" }}
+      onSubmit={() => {
+        console.log("Submitted!");
+        setOpened(false);
+      }}
+      render={props => (
+        <form className={className} onSubmit={props.handleSubmit}>
+          <input
+            type="text"
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+            value={props.values.title}
+            name="title"
+          />
+          {props.errors.title && <div id="feedback">{props.errors.title}</div>}
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    />
+  );
+};
 
 const Posts = ({ posts }) => (
   <div className="posts-container">
